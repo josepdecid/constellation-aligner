5a4c0c409acd41e220c21032b066006bd53ace0e
Josep de Cid <josep.de.cid@gmail.com>
2021-11-27 02:54:52 +0100

    Hide line and helper axis

--- src/main.ts
+++ src/main.ts
@@ -28,18 +28,16 @@ function init() {
 
 function setupWorld() {
     const dataPath = centerDataPoints(data)
-
-    let nextRandomizedZ = Math.random()
-    let randomizedZ = Math.random()
+    let previousRandomizedProjection: THREE.Vector3 = new THREE.Vector3()
 
     for (let i = 0; i < dataPath.length; i += SKIP_POINTS) {
         // Add points corresponding to the stars
         const material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, flatShading: true })
         const geometry = new THREE.DodecahedronGeometry(0.025, 10)
 
-        const randomizedProjection = getRandomAlignedPointToTarget(
-            new Vector3().copy(camera.position),
-            new Vector3(dataPath[i].x, dataPath[i].y, 0)
+        let randomizedProjection = getRandomAlignedPointToTarget(
+            new THREE.Vector3().copy(camera.position),
+            new THREE.Vector3(dataPath[i].x, dataPath[i].y, 0)
         )
 
         const mesh = new THREE.Mesh(geometry, material)
@@ -52,32 +50,33 @@ function setupWorld() {
         scene.add(mesh)
 
         // Add lines that join all the points with a parametrized alpha value
-        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF, transparent: true, opacity: 1, linewidth: 2 })
-        const lineGeometry = new THREE.BufferGeometry().setFromPoints([
-            new THREE.Vector3(dataPath[i].x, dataPath[i].y, 0),
-            camera.position
-            /*(i < dataPath.length - SKIP_POINTS)
-                ? new THREE.Vector3(dataPath[i + SKIP_POINTS].x, dataPath[i + SKIP_POINTS].y, randomizedZ)
-                : new THREE.Vector3(dataPath[0].x, dataPath[0].y, nextRandomizedZ)*/
-        ])
+        if (i > 0) {
+            const lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF, transparent: true, opacity: 1 })
+            const lineGeometry = new THREE.BufferGeometry().setFromPoints([
+                previousRandomizedProjection,
+                randomizedProjection
+            ])
 
-        const linePoint = new THREE.Line(lineGeometry, lineMaterial)
+            const linePoint = new THREE.Line(lineGeometry, lineMaterial)
 
-        lineSegments.push(linePoint)
-        scene.add(linePoint)
+            lineSegments.push(linePoint)
+            scene.add(linePoint)
+        }
 
-        randomizedZ = nextRandomizedZ
-        nextRandomizedZ = Math.random()
+        previousRandomizedProjection = randomizedProjection
     }
+
+    // TODO: Set random initial position for x and z sin and cos with y=0
+    // camera.position.copy(new Vector3())
 }
 
 function setupScene() {
     scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2(0x350089, 0.002);
 
-    const axesHelper = new THREE.AxesHelper();
+    /* const axesHelper = new THREE.AxesHelper();
     axesHelper.setColors(0xff0000, 0x00ff00, 0x0000ff)
-    scene.add(axesHelper);
+    scene.add(axesHelper); */
 
     const loader = new THREE.TextureLoader();
     const bgTexture = loader.load('resources/images/sky.jpg');
@@ -130,12 +129,12 @@ function setupIllumination() {
 
 function checkAlignmentAndShowLines() {
     const cameraDirection = camera.getWorldDirection(new THREE.Vector3(0, 0, 0))
-    const cameraAngleDistance = cameraDirection.distanceTo(new Vector3(0, 0, -1))
-    /*lineSegments.forEach(lineSegment => {
+    const cameraAngleDistance = cameraDirection.distanceTo(new THREE.Vector3(0, 0, -1))
+    lineSegments.forEach(lineSegment => {
         (lineSegment.material as THREE.Material).opacity = cameraAngleDistance < 0.5
             ? 2 * (0.5 - cameraAngleDistance) * 0.2
             : 0
-    })*/
+    })
 }
 
 function onWindowResize() {
